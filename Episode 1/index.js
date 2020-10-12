06aefb136850757a3099ba1ec914f7ce556bdd99
Naman Sharma <websitedesigner123456@gmail.com>
2020-10-12 23:17:16 +0530

    Episode One

---
+++ Episode 1/index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+
+
+client.once('ready', ()=>{
+
+    console.log('This bot is on!!')
+})
+
+
+
+client.login("NzY1MjY2MzkyNzA1NTMxOTU0.X4ST3Q.nzo-GLi7Jk5-IdbbuIsjyKH8yrI")
+
+
+// Your bot should now work!
+// If it does we will add a simple command ping
